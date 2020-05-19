ea3f3367ee141ec5feb06f9d791be0087f0571d1
unknown <kenadwanderson@gmail.com>
2020-05-19 15:02:37 -0300

    -Projeto inicial

---
+++ src/main/java/com/fullstack/catalagoHerois/CatalagoHeroisApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.catalagoHerois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalagoHeroisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalagoHeroisApplication.class, args);
+	}
+
+}
